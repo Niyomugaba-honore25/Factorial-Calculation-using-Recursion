dd992ff311ed51f6974fe0edc1e0fa96723bab9d
niyomugaba-honore25 <niyomugabahonore@gmail.com>
2026-07-12 20:59:52 +0200

    Add files via upload

---
+++ FactorialRecursion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+import java.util.Scanner;
+
+public class FactorialRecursion {
+
+  
+    public static long factorial(int number) {
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+       
+        return number * factorial(number - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+       
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long result = factorial(number);
+            System.out.println("Factorial of " + number + " is: " + result);
+        }
+
+        input.close();
+    }
+}
